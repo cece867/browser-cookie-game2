f74078e26fd57a13edf55dbfe9bf648905343c47
cece867 <bearonslegacy@gmail.com>
2025-10-14 21:07:53 -0400

    update

--- css/app.js
+++ css/app.js
@@ -1,42 +1,42 @@
 
-const button = document.querySelector('round-button');
-const message = document.querySelector('#message');
+const button = document.getElementById('button');
+const message = document.getElementById('message');
+const clicksCounter = document.getElementById('clicksCount');
+
 
 let clicksCount = 0;
- round-button.addEventListener('click', () => {
-   count++;
- })
-if (count === 1) {
+ button.addEventListener('click', () => {
+clicksCount++;
+clicksCounter.textContent = clicksCount;
+
+
+// button.addEventListener('click', () => {
+   message.textContent = "You Win A Cookie!";
+
+  // console.log(clicksCounter);
+  if (clicksCount === 1) {
     message.textContent = "You win 1 cookie!";
   } else {
-    message.textContent = "You win ${count} cookies!";
+    message.textContent = `You win ${clicksCount} cookies!`;
   }
+  });
 
 
-const msg = document.getElementById('message');
-roundButtonElement.addEventListener('click', function () {
-   message.textContent = "You Win A Cookie!";
-  
-})
 
- const buttonElement = document.createElement('li)');
-const roundButtonElement = document.querySelector('message');
-roundButtonElementy.addEventListener(click, message => {
-  console.log('you win a cookie!', message );
-})
+// roundButtonElementy.addEventListener(click, message => {
+//   console.log('you win a cookie!', message );
+// })
 
 // message.addEventListener(click, message => {
 //   console.log(click, message);
 // })
 // const buttonListElement = document.querySelector('ul');
-let count = 0;
-const Click = () => {
-    clicksCount = clicksCount + 1;
-    console.log(clicksCount); 
-}
+
+    // clicksCount = clicksCount +;
+    // console.log(clicksCount); 
 
 
-const roundButton = document.querySelector('.button'); 
-// const style = document.createElement('style');
+
+// const roundButton = document.querySelector('.button'); 
+// // const style = document.createElement('style');
 // style.textContent = 
-  
